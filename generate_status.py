561b88a72023c3f249dc9dba5b61ecd73a0fbfa7
Maik Kulbe <info@linux-web-development.de>
2016-08-11 01:36:54 +0200

    Rewrite of spaceapi generation to new python script with plugin system

---
+++ generate_status.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import json
+from datetime import datetime
+
+import requests
+from jinja2 import Template
+
+import config
+
+api_data = json.loads(requests.get(config.API_URL).text)
+
+for plugin in config.PLUGINS:
+    try:
+        plugin_module = getattr(
+            __import__("plugins.%s" % plugin, plugin).__dict__[plugin], plugin
+        )
+    except Exception as e:
+        print(e)
+    api_data = plugin_module(api_data)
+
+
+# The next part will generate the HTML for the status page
+with open(config.SAVE_PATH, 'w') as f:
+    f.write(json.dumps(api_data))
+    f.close()
+
+if api_data['state']['open'] is True:
+    icon = api_data['icon']['open']
+else:
+    icon = api_data['icon']['closed']
+
+html_template = Template(open(config.TEMPLATE_PATH).read())
+html = html_template.render(
+    status_open=api_data['state']['open'],
+    status_lastchange=datetime.fromtimestamp(
+        int(api_data['state']['lastchange'])).strftime('%H:%M:%S %d.%m.%Y'),
+    status_message=api_data['state']['message'],
+    status_icon=icon,
+    logo=api_data['logo'],
+    url=api_data['url'],
+    location=api_data['location'],
+)
+
+with open(config.HTML_PATH, 'wb+') as html_out:
+    html_out.write(html)
---
+++ plugins/temperature.py
@@ -0,0 +1,27 @@
+# -*- coding: iso-8859-15 -*-
+
+import requests
+import json
+
+import config
+
+
+def temperature(data):
+    temperature = []
+    try:
+        for url in config.PLUGINS['temperature']['urls']:
+            temp_json = json.loads(requests.get(url).text)
+            for loc, temp in temp_json.items():
+                temperature_item = {
+                    'value': temp,
+                    'unit': '°C',
+                    'location': loc
+                }
+                temperature.append(temperature_item)
+    except Exception as e:
+        print("TEMPERATURE: Unexpected error")
+        print(e)
+
+    data['sensors']['temperature'] = temperature
+
+    return data
